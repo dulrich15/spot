219029f6bba7db705b9d9c89f77df744d682ed1c
dulrich15 <davidjulrich@gmail.com>
2015-11-03 06:13:05 +0000

    Fixed trailing slash issue

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -10,11 +10,11 @@ urlpatterns = [
 
     url(r'^show/$', views.show_page, name='root_page'),
 
-    url(r'^show(?P<url>(/[\w\-/]*))$', views.show_page, name='show_page'),
-    url(r'^full(?P<url>(/[\w\-/]*))$', views.show_full, name='show_full'),
-    url(r'^prnt(?P<url>(/[\w\-/]*))$', views.prnt_page, name='prnt_page'),
-    url(r'^edit(?P<url>(/[\w\-/]*))$', views.edit_page, name='edit_page'),
+    url(r'^show(?P<url>(/([\w\-]+/)*))$', views.show_page, name='show_page'),
+    url(r'^full(?P<url>(/([\w\-]+/)*))$', views.show_full, name='show_full'),
+    url(r'^prnt(?P<url>(/([\w\-]+/)*))$', views.prnt_page, name='prnt_page'),
+    url(r'^edit(?P<url>(/([\w\-]+/)*))$', views.edit_page, name='edit_page'),
     url(r'^post/$', views.post_page, name='post_page'),
     
-    url(r'^full(?P<url>(/[\w\-/]*))$', views.show_full, name='show_full'),
+    url(r'^full(?P<url>(/([\w\-]+/)*))$', views.show_full, name='show_full'),
 ]
