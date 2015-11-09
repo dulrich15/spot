0d6176d17c92b456b8e432f52cbb3465d593cc0d
Dave <davidjulrich@gmail.com>
2015-11-09 05:53:14 +0000

    Added classroom student model

--- apps/core/models.py
+++ apps/core/models.py
@@ -305,3 +305,27 @@ class Page(Model):
         
     class Meta:
         ordering = ['url']
+
+
+class Student(Model):
+    classroom = ForeignKey(Classroom)
+    user = ForeignKey(User)
+
+    @property
+    def last_name(self):
+        return self.user.last_name
+
+    @property
+    def first_name(self):
+        return self.user.first_name
+
+    @property
+    def full_name(self):
+        return '{self.last_name}, {self.first_name}'.format(self=self)
+
+    def __unicode__(self):
+        return '{self.full_name} in {self.classroom}'.format(self=self)
+
+    class Meta:
+        ordering = ['user__last_name', 'user__first_name']
+
