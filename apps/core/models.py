02c21327b5841f7a5315d86cd6868d46dd973b8d
Dave <davidjulrich@gmail.com>
2015-11-11 07:16:18 +0000

    Moved student model from core to grades app

--- apps/core/models.py
+++ apps/core/models.py
@@ -9,7 +9,6 @@ import os
 import posixpath
 import re
 
-from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from django.db.models import *
 
@@ -307,25 +306,3 @@ class Page(Model):
     class Meta:
         ordering = ['url']
 
-
-class Student(Model):
-    classroom = ForeignKey(Classroom)
-    user = ForeignKey(User)
-
-    @property
-    def last_name(self):
-        return self.user.last_name
-
-    @property
-    def first_name(self):
-        return self.user.first_name
-
-    @property
-    def full_name(self):
-        return '{self.last_name}, {self.first_name}'.format(self=self)
-
-    def __unicode__(self):
-        return '{self.full_name} in {self.classroom}'.format(self=self)
-
-    class Meta:
-        ordering = ['user__last_name', 'user__first_name']
