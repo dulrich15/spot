aaedd3c0402ac48be7ff583f341aac502f870878
Dave <davidjulrich@gmail.com>
2017-01-28 22:50:00 +0000

    Updates

--- apps/core/templatetags/docutils_extensions/matplotlib/pyplot.py
+++ apps/core/templatetags/docutils_extensions/matplotlib/pyplot.py
@@ -10,8 +10,8 @@ from scipy.spatial.distance import pdist, squareform
 import matplotlib
 from matplotlib import pyplot as plt
 
-from mpl_toolkits.mplot3d import Axes3D     # Import 3D plotting tools.
-from scipy.special import jn                # Import Bessel function.
+from mpl_toolkits.mplot3d.axes3d import Axes3D     # Import 3D plotting tools.
+# from scipy.special import jn                # Import Bessel function.
 
 # matplotlib.rcParams['xtick.direction'] = 'out'
 # matplotlib.rcParams['ytick.direction'] = 'out'
